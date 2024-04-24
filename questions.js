41311cec9fcfbf72014b097f1c8b5426f26dc2de
harshitk04 <149113598+harshitk04@users.noreply.github.com>
2024-04-24 14:10:25 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions=[
+    {
+        numb:1,
+        question:"Which type of defect involves an extra atom inserted into the crystal structure, often leading to increased hardness and strength in metallic materials?",
+        answer:"Interstitial defect",
+        options:[
+            "Interstitial defect",
+            "Vacancy defect",
+            "Frenkel defect",
+            "Schottky defect"
+            ]
+    },
+    {
+        numb:2,
+        question:"In metal alloys, what term describes the situation when different elements occupy equivalent crystal lattice sites, leading to distortion of the regular crystal structure?",
+        answer:"Substitutional solid solution",
+        options:[
+            "Grain boundary",
+            "Interstitial solid solution",
+            "Substitutional solid solution",
+            "Phase segregation"            
+        ]
+    },
+    {
+        numb:3,
+        question:"What is the primary cause of dislocation in a crystalline structure?",
+        answer:"Application of stress",        
+        options:[
+            "Introduction of impurities",
+            "Application of stress",
+            "Rapid cooling",        
+            "Annealing process"
+        ]
+    },
+    {
+        numb:4,
+        question:"Which of the following is NOT a type of point defect in a crystal lattice?",
+        answer:"Line defect",
+        options:[
+            "Vacancy defect",
+            "Interstitial defect",
+            "Line defect",
+            "Substitutional defect"
+        ]
+    },
+    {
+        numb:5,
+        question:"Which type of defect involves the simultaneous movement of atoms in a crystal lattice",
+        answer:"Slip bands",
+        options:[
+            "Slip bands",
+            "Frenkel defect",
+            "Schottky defect",
+            "Grain boundary"
+        ]
+    }
+]
